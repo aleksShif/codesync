d16bb88ce5f1f7113ef4ea713a1010f96352fd61
Aleksandra Shifrina <sasha.shifrina05@gmail.com>
2026-05-04 20:13:38 -0400

    fixing

--- frontend/src/pages/RepoDetailsPage.js
+++ frontend/src/pages/RepoDetailsPage.js
@@ -49,7 +49,7 @@ const RepoDetailsPage = () => {
     const uniqueBranchViewers = React.useMemo(() => {
         const unique = new Map();
         Object.values(activeDevs).forEach(devArray => {
-            devArray.forEach(dev => unique.set(dev.id, dev));
+            devArray.forEach(dev => unique.set(dev.author, dev));
         });
         return Array.from(unique.values());
     }, [activeDevs]);
